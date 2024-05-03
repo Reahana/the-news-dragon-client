3431ca038d4eab00b54271e662c616741cf86fb6
Reahana <reahana233@gmail.com>
2024-05-03 12:24:37 +0600

    layout

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-center'>Thhis is footer</p>
+        </div>
+    );
+};
+
+export default Footer;
