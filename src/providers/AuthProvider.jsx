ac2751974adc30b04bd1f00617c15f23bfb95b00
Reahana <reahana233@gmail.com>
2024-05-04 23:16:32 +0600

    login

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,17 +1,40 @@
 import React, { createContext } from 'react';
-import { getAuth } from "firebase/auth";
 import app from '../firebase/firebase.config';
-
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
-    const user = null;
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);  
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
     
     const authInfo = {
-        user
+    
+        user,
+        loading,
+        createUser,
+        signIn,
+        logOut
     }
 
     return (
